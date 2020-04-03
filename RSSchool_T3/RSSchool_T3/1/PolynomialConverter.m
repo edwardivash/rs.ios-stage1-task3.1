c71c7d449f5981d459eee14a0529c3aea7e42aee
Eduard Ivash <eduardivash@gmail.com>
2020-04-03 19:35:02 +0300

    Add solution for task1

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,34 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    
+    NSMutableString *str = [[NSMutableString alloc]init];
+    
+    NSInteger count = numbers.count;
+    
+    if ([numbers isEqual:@[]]) {
+        return nil;
+    }
+    
+    for (int i = 0; i < numbers.count-1; i++) {
+        count--;
+        int num1 = numbers[i].intValue;
+        if (num1 < 0) {
+            [str appendFormat:@" - %dx^%ld", numbers[i].intValue * -1, count];
+        }
+        
+        if (num1 > 0) {
+            [str appendFormat:@" + %dx^%ld", numbers[i].unsignedIntValue,count];
+        }
+        
+        
+    }
+    
+    if (numbers.lastObject.intValue != 0) {
+        [str appendFormat:@" + %@",numbers.lastObject];
+    }
+    
+    return [[[str substringFromIndex:3]stringByReplacingOccurrencesOfString:@"^1" withString:@""]stringByReplacingOccurrencesOfString:@"1x" withString:@"x"];
 }
+
 @end
