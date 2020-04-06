df647a2fed684ad5a17d740ad1c512450fb08e2a
Eduard Ivash <eduardivash@gmail.com>
2020-04-06 21:46:11 +0300

    Add task3

--- RSSchool_T3/RSSchool_T3/3 /ViewController.m
+++ RSSchool_T3/RSSchool_T3/3 /ViewController.m
@@ -30,9 +30,7 @@
     [self setupTextFields];
     [self setupButton];
     [self setupAccessibility];
-    
 }
-
 
 // Setup resultViewColorView
 -(void)setupResultViewColorView {
@@ -47,7 +45,6 @@
     self.labelResultColor = [[UILabel alloc]initWithFrame:CGRectMake(30, 100, 90, 30)];
     [self.labelResultColor setText:@"Color"];
     self.labelResultColor.textColor = [UIColor blackColor];
-    
     [self.view addSubview:self.labelResultColor];
     
     // labelRed
@@ -55,7 +52,7 @@
     [self.labelRed setText:@"RED"];
     self.labelRed.textColor = [UIColor redColor];
     [self.view addSubview:self.labelRed];
-
+    
     // labelGreen
     self.labelGreen = [[UILabel alloc]initWithFrame:CGRectMake(30, 240, 70, 30)];
     [self.labelGreen setText:@"GREEN"];
@@ -67,8 +64,6 @@
     [self.labelBlue setText:@"BLUE"];
     self.labelBlue.textColor = [UIColor blueColor];
     [self.view addSubview:self.labelBlue];
-
-
 }
 
 // Setup textFields
@@ -77,24 +72,27 @@
     self.textFieldRed = [[UITextField alloc]initWithFrame:CGRectMake(120, 170, 220, 30)];
     [self.textFieldRed setPlaceholder:@"0..255"];
     self.textFieldRed.borderStyle = round(3);
+    self.textFieldRed.keyboardType = UIKeyboardTypeNumberPad;
     [self.view addSubview:self.textFieldRed];
     
     // textFieldGreen
     self.textFieldGreen = [[UITextField alloc]initWithFrame:CGRectMake(120, 240, 220, 30)];
     [self.textFieldGreen setPlaceholder:@"0..255"];
     self.textFieldGreen.borderStyle = round(3);
+    self.textFieldGreen.keyboardType = UIKeyboardTypeNumberPad;
     [self.view addSubview:self.textFieldGreen];
     
     // textFieldBlue
     self.textFieldBlue = [[UITextField alloc]initWithFrame:CGRectMake(120, 310, 220, 30)];
     [self.textFieldBlue setPlaceholder:@"0..255"];
     self.textFieldBlue.borderStyle = round(3);
+    self.textFieldBlue.keyboardType = UIKeyboardTypeNumberPad;
     [self.view addSubview:self.textFieldBlue];
 }
 
-
 // Setup button
 -(void)setupButton {
+    // Button customisation
     self.buttonProcess = [[UIButton alloc]initWithFrame:CGRectMake(self.view.frame.size.width / 2 - 35, 380, 70, 30)];
     [self.buttonProcess setTitle:@"Process" forState:UIControlStateNormal];
     [self.buttonProcess setTitleColor:UIColor.systemBlueColor forState:UIControlStateNormal];
@@ -102,10 +100,41 @@
     [self.view addSubview:self.buttonProcess];
 }
 
-
 // Great Mixer
 -(void)greatMixer {
+    int red = self.textFieldRed.text.intValue;
+    int green = self.textFieldGreen.text.intValue;
+    int blue = self.textFieldBlue.text.intValue;
+    NSCharacterSet* numbers = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
     
+    if ( [self.textFieldGreen.text rangeOfCharacterFromSet:numbers].location != NSNotFound ||
+        [self.textFieldBlue.text rangeOfCharacterFromSet:numbers].location != NSNotFound ||
+        [self.textFieldRed.text rangeOfCharacterFromSet:numbers].location != NSNotFound ||
+        [self.textFieldRed.text  isEqual: @""] || [self.textFieldGreen.text  isEqual: @""] ||
+        [self.textFieldBlue.text  isEqual: @""] ||
+        red <= 0 || red >= 255 || green <= 0 || green >= 255 || blue <= 0 || blue >= 255) {
+        self.labelResultColor.text = @"Error";
+        self.viewResultColor.backgroundColor = [UIColor clearColor];
+    } else {
+        UIColor *color = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:1];
+        self.viewResultColor.backgroundColor = color;
+        self.labelResultColor.text = [self hexFromColor:color];
+        
+        // Clear textFields
+        self.textFieldRed.text = @"";
+        self.textFieldGreen.text = @"";
+        self.textFieldBlue.text = @"";
+        // Hide keyboard
+        [self.textFieldRed resignFirstResponder];
+        [self.textFieldGreen resignFirstResponder];
+        [self.textFieldBlue resignFirstResponder];
+    }
+}
+
+// GetHex
+-(NSString *)hexFromColor:(UIColor*)color {
+    const CGFloat *components = CGColorGetComponents(color.CGColor);
+    return [NSString stringWithFormat:@"0x%02lX%02lX%02lX",lroundf(components[0] * 255.0),lroundf(components[1] * 255.0),lroundf(components[2] * 255.0)];
 }
 
 // Accessibility
